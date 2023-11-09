4f573777e7d0054d7a8b9d98ff3e2cedcf984333
Harpreet Singh <harpreetsingh70159@gmail.com>
2023-11-09 14:35:05 +0530

    custom text

--- lib/customs/custom_text.dart
+++ lib/customs/custom_text.dart
@@ -1,41 +1,32 @@
-
-
 import 'dart:ffi';
 
 import 'package:flutter/material.dart';
 
 class CustomText extends StatelessWidget {
-
   String mText;
   double? mSize;
   FontWeight? mWeight;
   Color? mColor;
   String? mFamily;
 
-
-
-
-   CustomText({
+  CustomText({
     required this.mText,
-     this.mSize,
-     this.mWeight,
-     this.mColor,
-     this.mFamily = "Roboto",
-});
+    this.mSize,
+    this.mWeight,
+    this.mColor,
+    this.mFamily = "Roboto",
+  });
 
   @override
   Widget build(BuildContext context) {
     return Text(
       mText,
       style: TextStyle(
-
         fontSize: mSize,
         fontWeight: mWeight,
         color: mColor,
         fontFamily: mFamily,
-
       ),
-
     );
   }
 }
